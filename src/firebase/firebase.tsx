b0f4979037ecf96e9ac8b0932445dbdc02f9ebe5
Biprodeep12 <bipbose123@gmail.com>
2025-02-25 02:30:02 +0530

    kol

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -2,16 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA0xwE6zWd8cthVtwBdZFVQcaWLw66cEpA',
-  authDomain: 'calendar12-736e3.firebaseapp.com',
-  databaseURL:
-    'https://calendar12-736e3-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'calendar12-736e3',
-  storageBucket: 'calendar12-736e3.firebasestorage.app',
-  messagingSenderId: '395909597',
-  appId: '1:395909597:web:77ce885b7093d77c943e34',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
